62e923886362b9c2d9ed8b51c88d3d63535c05b5
Nikita Morozov <nikitamorozka@gmail.com>
2025-04-03 04:34:51 +0400

    ADD SWAGGER

--- V2/src/main/java/org/javaguru/travel/insurance/core/services/TravelCalculatePremiumServiceImpl.java
+++ V2/src/main/java/org/javaguru/travel/insurance/core/services/TravelCalculatePremiumServiceImpl.java
@@ -8,23 +8,21 @@ import org.javaguru.travel.insurance.core.api.dto.AgreementDTO;
 import org.javaguru.travel.insurance.core.api.dto.ValidationErrorDTO;
 import org.javaguru.travel.insurance.core.validations.TravelAgreementValidator;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
 
 @Component
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
 
     private final TravelAgreementValidator agreementValidator;
-
     private final CalculateRisksForPremium calculateRisksForPremium;
-
     private final CalculateTotalPremiumAgreement totalPremiumAgreement;
-
     private final AgreementSaverService agreementSaverService;
 
-
+    @Transactional(isolation = Isolation.READ_COMMITTED)
     public TravelCalculatePremiumCoreResult calculatePremium(TravelCalculatePremiumCoreCommand command) {
         List<ValidationErrorDTO> errors = agreementValidator.validate(command.getAgreement());
         if (errors.isEmpty()) {
